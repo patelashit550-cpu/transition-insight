bdd97eadb376c91b394ca0442d9423f06203103c
Ashit Milne <patelashit550@gmail.com>
2026-08-11 12:29:50 -0400

    Save uncommitted changes

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,8 +1,10 @@
+import { CompassWatermark } from "@/components/features/CompassWatermark";
 import { HomeBento } from "@/components/features/HomeBento";
 
 export default function Home() {
   return (
     <div className="p3-landing-home flex flex-1 flex-col justify-start w-full relative">
+      <CompassWatermark />
       <div className="relative w-full" style={{ zIndex: 1 }}>
         <HomeBento />
       </div>
